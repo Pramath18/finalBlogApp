f3c76d2e37ece9b424e3e3cd64c28d8715d66a0f
pramath <pramathga@gmail.com>
2021-05-21 11:25:24 +0530

    final commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,10 @@
 class CommentsController < ApplicationController
-    # def index
-    #     @comment = Comments.all
-    # end
+     def index
+         @comment = Comments.all
+     end
     def create
         @post = Post.find(params[:post_id])
-        @comment = @post.comments.create(comment_params)
+        @comment = @post.comments.create(comment_params.merge(user_id: current_user.id))
         redirect_to post_path(@post)
     end
     def destroy
@@ -14,6 +14,6 @@ class CommentsController < ApplicationController
         redirect_to post_path(@post)
     end
     private def comment_params
-     params.require(:comment).permit(:username, :body)
+     params.require(:comment).permit(:body)
     end
 end
