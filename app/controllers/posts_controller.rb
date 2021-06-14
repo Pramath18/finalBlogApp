9872172090e32e11afa7e9dae8f52a35441f86fc
pramath <pramathga@gmail.com>
2021-06-14 21:13:52 +0530

    removed errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,7 +36,7 @@ class PostsController < ApplicationController
   # PST /posts or /posts.json
   def create
     @post = Post.new(post_params)
-    @post.category_id = params[:category_id]
+     @post.category_id = params[:category_id]
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: "Post was successfully created." }
@@ -84,7 +84,6 @@ class PostsController < ApplicationController
     def set_post
       @post = Post.find(params[:id])
     end
-    def
     # Only allow a list of trusted parameters through.
     def post_params
       params.require(:post).permit(:title, :body, :category_id, :image, :private)
